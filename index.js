776e2d4efecacaf709be88084937725c73470aec
facundoverdasco <166016911+facundoverdasco@users.noreply.github.com>
2024-08-12 08:08:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//Actividad 1
+let judaroes = ["facu","santi","sosa","anna","maxi"]
+
+//Actividad 2
+let puntajes = []
+for (let index = 0; index < 5; index=index+1) {
+ puntajes.push = prompt ("Ingresa un puntaje")
+}
+
+//Actividad 3
+let maxpuntaje = puntaje [0]
+let jugadormax = jugadores[0]
+for (let puntajes = 0; puntajes < puntajes.length; index++) {
+    
+    
+}
